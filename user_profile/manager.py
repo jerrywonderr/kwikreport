a67069f4d6faa8e2a875c487cd668737991d1c2a
Jerry Wonder <jerrycul2001@gmail.com>
2023-05-19 14:21:15 +0100

    feat: :sparkles: implement custom user model

---
+++ user_profile/manager.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Method to create a regular user
+        """
+        if not email:
+            raise ValueError("Email must be provided")
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        """
+        Method to create a super user
+        """
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
