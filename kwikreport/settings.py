66934f0bea7cacb53c5c596d816fd472f4530494
Jerry Wonder <jerrycul2001@gmail.com>
2023-05-25 10:50:49 +0100

    feat: :sparkles: protect home route

--- kwikreport/settings.py
+++ kwikreport/settings.py
@@ -143,4 +143,5 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
